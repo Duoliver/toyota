c825faffda830d9d27f6f1908b84f22172e1942b
Honeysalad <oliveira.douglasedu@outlook.com>
2019-11-26 10:12:22 -0300

    Modularized dashboard navbar

--- php/add_login.php
+++ php/add_login.php
@@ -16,30 +16,9 @@
 
 <body>
 
-    <nav>
-        <div class="container">
-            <div class="nav-wrapper">
-                <a href="../index.php" class="brand-logo"><img src="../img/logo.png" alt="TOYOTA"></a>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul class="right hide-on-med-and-down">
-                    <li><a href="cadastros.php">CADASTROS</a></li>
-                    <li><a href="add_login.php">ADICIONAR LOGIN</a></li>
-                    <li><a href="editar_login.php">EDITAR LOGIN</a></li>
-                    <li><a href="excluir_login.php">EXCLUIR LOGIN</a></li>
-                    <li><a href="relatorio.php">RELATÓRIO</a></li>
-                    <li><a href="logout.php">LOGOUT</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <ul class="sidenav" id="mobile-demo">
-        <li><a href="cadastros.php">CADASTROS</a></li>
-        <li><a href="add_login.php">ADICIONAR LOGIN</a></li>
-        <li><a href="editar_login.php">EDITAR LOGIN</a></li>
-        <li><a href="excluir_login.php">EXCLUIR LOGIN</a></li>
-        <li><a href="relatorio.php">RELATÓRIO</a></li>
-        <li><a href="logout.php">LOGOUT</a></li>
-    </ul>
+    <?php
+        require('navbar_dashboard.php');
+    ?>
 
     <div class="container">
 
---
+++ php/navbar_dashboard.php
@@ -0,0 +1,22 @@
+<nav>
+    <div class="container">
+        <div class="nav-wrapper">
+            <a href="../index.php" class="brand-logo"><img src="../img/logo.png" alt="TOYOTA"></a>
+            <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="cadastros.php">CADASTROS</a></li>
+                <li><a href="add_login.php">ADICIONAR LOGIN</a></li>
+                <li><a href="admins.php">ADMINS</a></li>
+                <li><a href="relatorio.php">RELATÓRIO</a></li>
+                <li><a href="logout.php">LOGOUT</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<ul class="sidenav" id="mobile-demo">
+    <li><a href="cadastros.php">CADASTROS</a></li>
+    <li><a href="add_login.php">ADICIONAR LOGIN</a></li>
+    <li><a href="admins.php">ADMINS</a></li>
+    <li><a href="relatorio.php">RELATÓRIO</a></li>
+    <li><a href="logout.php">LOGOUT</a></li>
+</ul>
